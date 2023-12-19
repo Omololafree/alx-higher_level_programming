58791c72da34c293ef2f45caf34c817d3b02b6a0
Omololafree <nancydson@gmail.com>
2023-12-19 17:26:36 +0000

    rest

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+mport sys
+
+
+def safe_function(fct, *args):
+    try:
+        num = fct(*args)
+        return num
+    except Exception as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return None
